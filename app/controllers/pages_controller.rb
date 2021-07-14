44b1ea81c0a84d2e350fa043f83f67189f488ab2
kauredo <vaskafig@gmail.com>
2021-07-14 17:39:58 +0100

    fix next and prev pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
   def show
     @page = Page.friendly.find(params[:slug])
     redirect_to root_path if @page.nil?
-    @next_page = Page.where('pages.order > ?', @page.order).first
-    @prev_page = Page.where('pages.order < ?', @page.order).last
+    @next_page = Page.where('pages.order > ?', @page.order).order(:order).first
+    @prev_page = Page.where('pages.order < ?', @page.order).order(:order).last
   end
 end
